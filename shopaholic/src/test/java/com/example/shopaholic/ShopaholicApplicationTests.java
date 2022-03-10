10b67ee30c87b1e31d5ea3061be05e84012ac7ec
csardana <35585694+csardana@users.noreply.github.com>
2022-03-10 16:14:24 -0500

    Add files via upload

---
+++ shopaholic/src/test/java/com/example/shopaholic/ShopaholicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopaholic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class shopaholicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
